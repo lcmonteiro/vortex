22b91a704a9e9aac1ece8fa366ac79c4dd6ab20b
lcmonteiro <monteiro.lcm@gmail.com>
2026-08-06 16:35:33 +0100

    build: enable compile_commands.json export; re-apply IWYU pragma exports on umbrella headers

--- sources/foundation/graph.hpp
+++ sources/foundation/graph.hpp
@@ -6,9 +6,9 @@
 #ifndef VORTEX_FOUNDATION_GRAPH_GRAPH_HPP
 #define VORTEX_FOUNDATION_GRAPH_GRAPH_HPP
 
-#include "foundation/graph/config.hpp"
-#include "foundation/graph/container.hpp"
-#include "foundation/graph/edge.hpp"
-#include "foundation/graph/node.hpp"
+#include "foundation/graph/config.hpp"     // IWYU pragma: export
+#include "foundation/graph/container.hpp"  // IWYU pragma: export
+#include "foundation/graph/edge.hpp"       // IWYU pragma: export
+#include "foundation/graph/node.hpp"       // IWYU pragma: export
 
 #endif  // VORTEX_FOUNDATION_GRAPH_GRAPH_HPP
--- sources/foundation/math.hpp
+++ sources/foundation/math.hpp
@@ -6,7 +6,7 @@
 #ifndef VORTEX_FOUNDATION_MATH_MATH_HPP
 #define VORTEX_FOUNDATION_MATH_MATH_HPP
 
-#include "foundation/math/solver.hpp"
-#include "foundation/math/types.hpp"
+#include "foundation/math/solver.hpp"  // IWYU pragma: export
+#include "foundation/math/types.hpp"   // IWYU pragma: export
 
 #endif  // VORTEX_FOUNDATION_MATH_MATH_HPP
